131d343cb031223ad64f54c77925e1c56b0934bd
devproo <ipeerless0@gmail.com>
2023-06-18 21:53:23 +0300

    Initial Commit

---
+++ weather-app-swiftui/weather_app_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  weather_app_swiftuiApp.swift
+//  weather-app-swiftui
+//
+//  Created by ipeerless on 09/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct weather_app_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
